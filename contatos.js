18664b58453cd97a20ca081f7f0eb4aa4198ca20
Daniele-SS <dev.daniele@outlook.com>
2026-05-18 08:29:29 -0300

    feat: adiciona as duas formas de criação da minha estrutua condicional

--- contatos.js
+++ contatos.js
@@ -2,6 +2,15 @@ const URL = 'https://bakcend-fecaf-render.onrender.com/contatos'
 
 export async function getContatos() {
     const response = await fetch (URL)
-    if(!response.ok) throw new Error('Erro ao buscar contatos') //Se não estiver OK é porque deu erro. O '!' significa uma negativa no JavaScript
-    return response.json()
+
+    //Primeira opção para criar o if else
+    // if(!response.ok) throw new Error('Erro ao buscar contatos') //Se não estiver OK é porque deu erro. O '!' significa uma negativa no JavaScript
+    // return response.json()
+
+    //Segunda opção para criar o if else
+    if(!response.ok) {
+        return response.json()
+    } else {
+        throw new Error('Erro ao buscar contatos')
+    }
 }
