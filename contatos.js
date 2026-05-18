e7dffea20ef72bc608a56ebbf6649099723d4e38
Daniele-SS <dev.daniele@outlook.com>
2026-05-18 08:26:42 -0300

    feat: adiciona os a function que retorna erro

--- contatos.js
+++ contatos.js
@@ -1,1 +1,7 @@
-'use strict'
+const URL = 'https://bakcend-fecaf-render.onrender.com/contatos'
+
+export async function getContatos() {
+    const response = await fetch (URL)
+    if(!response.ok) throw new Error('Erro ao buscar contatos') //Se não estiver OK é porque deu erro. O '!' significa uma negativa no JavaScript
+    return response.json()
+}
